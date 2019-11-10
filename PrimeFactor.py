6f2337104a611c8e2433ed11d7ce7158f26dde91
Jo Bleu <johannasabsantos@gmail.com>
2019-11-10 00:28:32 -0300

    prime factor and list

---
+++ PrimeFactor.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+
+def testB(self);
+    self.assertEqual(0,len(prime_factors_of(1)))
+    def prime_factors_of(self,i);
+        return []
+
+        
---
+++ lista.py
@@ -0,0 +1,6 @@
+lista = ["come","and", "mess","my"]
+print(len(lista))
+txt = ""
+for i in lista:
+    txt= txt + i 
+print (txt) 
